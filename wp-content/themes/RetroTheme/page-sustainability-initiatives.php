ac7980313d62324789379f25a88da79108a5eeac
LunaDamm <lunand01@easv365.dk>
2025-09-11 09:38:50 +0200

    Update page-sustainability-initiatives.php

--- wp-content/themes/RetroTheme/page-sustainability-initiatives.php
+++ wp-content/themes/RetroTheme/page-sustainability-initiatives.php
@@ -3,10 +3,26 @@
 ?>
 <?php get_header(); ?>
 
+<?php
+            $title = get_the_title();
+            $content = get_the_content();
+            ?>
+
     <?php if(have_posts()): ?>
         <?php while(have_posts()): the_post(); ?>
-            <h1><?php the_title(); ?></h1>
-            <div><?php the_content(); ?></div>
+            <h1 class="text-8xl font-bold mb-6 text-white font-headline"><?php echo esc_html($title); ?></h1>
+
+            <div class="rounded-lg overflow-hidden" style="background-color: #D6D6D6;">
+                <?php if (has_post_thumbnail()): ?>
+                    <div class="w-full not-prose">
+                        <?php the_post_thumbnail('large', ['class' => 'w-full h-auto']); ?>
+                    </div>
+                <article class="prose max-w-none p-8 text-black">
+                    <div class="text-black">
+                        <?php echo $content; ?>
+                    </div>
+                </article>
+                <?php endif; ?>
         <?php endwhile; ?>
     <?php endif; ?>
 
