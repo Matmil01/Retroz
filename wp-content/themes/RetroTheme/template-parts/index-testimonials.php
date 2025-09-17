7b7f40af4e1e866c73ebd6cb7efc4c87381e39b5
LunaDamm <lunand01@easv365.dk>
2025-09-17 10:13:08 +0200

    alttekst

--- wp-content/themes/RetroTheme/template-parts/index-testimonials.php
+++ wp-content/themes/RetroTheme/template-parts/index-testimonials.php
@@ -20,7 +20,7 @@
 ?>
 
 <section class="testimonial bg-[#D9D9D9] p-4 flex flex-col items-center text-center rounded-lg">
-    <img class="h-69" src="<?php echo esc_url($testimonialImg['url']); ?>" alt="">
+    <img class="h-69" src="<?php echo esc_url($testimonialImg['url']); ?>" alt="<?php echo esc_attr($testimonialImg['alt']); ?>" />
     <div class="bg-[#B3B3B3] mt-4 px-4 rounded-lg">
         <h1 class=""><?php echo esc_html($testimonialName); ?></h1>
         <p class=""><?php echo esc_html($testimonialText); ?></p>
