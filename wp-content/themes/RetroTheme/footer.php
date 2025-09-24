696a6de03767795c1fbceb94b37505f11470c5e6
Matmil01 <matmil01@easv365.dk>
2025-09-24 10:44:18 +0200

    Update footer.php

--- wp-content/themes/RetroTheme/footer.php
+++ wp-content/themes/RetroTheme/footer.php
@@ -4,15 +4,15 @@
     <div class="max-w-6xl mx-auto px-6 flex items-center justify-between font-main text-white">
         <p>© <?php echo date('Y'); ?> Team Skærebært</p>
         <div class="flex items-center gap-10">
-            <a href="https://theuselessweb.com/" target="_blank" rel="noopener" class="w-5 h-5">
+            <span class="w-5 h-5 cursor-pointer">
                 <?php echo file_get_contents(get_template_directory() . '/icons/instagram.svg'); ?>
-            </a>
-            <a href="https://theuselessweb.com/" target="_blank" rel="noopener" class="w-5 h-5">
+            </span>
+            <span class="w-5 h-5 cursor-pointer">
                 <?php echo file_get_contents(get_template_directory() . '/icons/tiktok.svg'); ?>
-            </a>
-            <a href="https://theuselessweb.com/" target="_blank" rel="noopener" class="w-5 h-5">
+            </span>
+            <span class="w-5 h-5 cursor-pointer">
                 <?php echo file_get_contents(get_template_directory() . '/icons/youtube.svg'); ?>
-            </a>
+            </span>
         </div>
     </div>
 </footer>
