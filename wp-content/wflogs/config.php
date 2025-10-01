3102bfc43ca1f76af5daa638694d2252d7aacc6a
Mirkel1998 <mikped01@easv365.dk>
2025-10-01 09:52:44 +0200

    2 factor + password + user/members
    Noice

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1759908972;s:7:"authKey";s:64:"X!h,rt/sHMuQy/V^{u<0E&rT8/y}8h!g+4n<VL4B.G)6H#5Pur<p_$Rzg)+%N5[;";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:912;}
