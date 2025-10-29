27d4d8c17d3097c77c5b448b66435d9105320e48
LunaDamm <lunand01@easv365.dk>
2025-10-29 13:23:29 +0100

    fuck wordpress

--- wp-content/themes/RetroTheme/footer.php
+++ wp-content/themes/RetroTheme/footer.php
@@ -3,7 +3,7 @@
 <footer class="w-full py-8">
     <div class="max-w-6xl mx-auto px-6 flex items-center justify-between font-main text-white">
         <p>© <?php echo date('Y'); ?> Team Skærebært</p>
-        <a href="http://retroz.local/wp-content/uploads/2025/10/infograf-password.pdf" class="text-white">Password Helper</a>
+        <a href="http://thememorycard.matmil.dk/wp-content/uploads/2025/10/infograf-password.pdf" class="text-white">Password Helper</a>
         <div class="flex items-center gap-10">
             <a href="https://theuselessweb.com" target="_blank" rel="noopener" class="w-5 h-5 cursor-pointer" alt="The Useless Web">
                 <span>
